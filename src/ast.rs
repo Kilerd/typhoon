ec6f38f2bd84198d345c79f90dfdee416b9aecc9
Kilerd Chan <blove694@gmail.com>
2020-05-12 17:54:40 +0800

    feat: impl arithmetic logic codegen.

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,7 @@
+use llvm_sys::core::{LLVMInt32TypeInContext, LLVMConstInt, LLVMBuildOr, LLVMBuildXor, LLVMBuildAnd, LLVMBuildBinOp, LLVMBuildLShr, LLVMBuildShl, LLVMBuildAShr, LLVMBuildMul, LLVMBuildUDiv, LLVMBuildAdd, LLVMBuildSub, LLVMBuildSDiv};
+use llvm_sys::prelude::{LLVMBuilderRef, LLVMContextRef, LLVMValueRef};
+use llvm_sys::LLVMOpcode::LLVMOr;
+
 #[derive(Debug)]
 
 // stmt
@@ -25,6 +29,82 @@ pub enum Expr {
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
     Pow(Box<Expr>, Box<Expr>),
+}
+
+impl Expr {
+    pub unsafe fn codegen(&self, context: LLVMContextRef, builder: LLVMBuilderRef) -> LLVMValueRef {
+        match self {
+            Expr::Number(n) => {
+                let int_type = LLVMInt32TypeInContext(context);
+                let int = LLVMConstInt(int_type, *n as u64, 0);
+                int
+            },
+            Expr::Identifier(_) => {
+                unimplemented!()
+            }
+            Expr::Or(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildOr(builder, lhs_value, rhs_value, c_str!("ortmp"))
+            }
+            Expr::Xor(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildXor(builder, lhs_value, rhs_value, c_str!("xortmp"))
+            }
+            Expr::And(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildAnd(builder, lhs_value, rhs_value, c_str!("andtmp"))
+            }
+            Expr::LShift(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildShl(builder, lhs_value, rhs_value, c_str!("lshifttmp"))
+            }
+            Expr::RShift(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildAShr(builder, lhs_value, rhs_value, c_str!("rshifttmp"))
+            }
+            Expr::Mod(lhs, rhs) => {
+
+                let lhs_value_1 = lhs.codegen(context, builder);
+
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+
+                let div = LLVMBuildSDiv(builder, lhs_value, rhs_value, c_str!("modinnerdivtmp"));
+                let mul = LLVMBuildMul(builder, div, rhs_value, c_str!("modinnermultmp"));
+                LLVMBuildSub(builder, lhs_value_1, mul, c_str!("modtmp"))
+            }
+            Expr::Mul(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildMul(builder, lhs_value, rhs_value, c_str!("multmp"))
+            }
+            Expr::Div(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildSDiv(builder, lhs_value, rhs_value, c_str!("udivtmp"))
+            }
+            Expr::Add(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildAdd(builder, lhs_value, rhs_value, c_str!("addtmp"))
+            }
+            Expr::Sub(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                LLVMBuildSub(builder, lhs_value, rhs_value, c_str!("subtmp"))
+            }
+            Expr::Pow(lhs, rhs) => {
+                let lhs_value = lhs.codegen(context, builder);
+                let rhs_value = rhs.codegen(context, builder);
+                lhs_value
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use crate::ast::Expr;
 use lalrpop_util::lalrpop_mod;
 use llvm_sys::core;
-use llvm_sys::core::LLVMPrintValueToString;
+use llvm_sys::core::{LLVMPrintModuleToString, LLVMPrintValueToString};
 use llvm_sys::target::{
     LLVM_InitializeAllAsmParsers, LLVM_InitializeAllAsmPrinters, LLVM_InitializeAllTargetInfos,
     LLVM_InitializeAllTargetMCs, LLVM_InitializeAllTargets, LLVM_InitializeNativeTarget,
@@ -12,27 +13,48 @@ use llvm_sys::target_machine::{
 };
 use std::ffi::{CStr, CString};
 use std::ptr;
-
-pub mod ast;
-lalrpop_mod!(pub parser);
+use structopt::StructOpt;
 
 macro_rules! c_str {
     ($s:expr) => {
         concat!($s, "\0").as_ptr() as *const i8
     };
 }
+pub mod ast;
+lalrpop_mod!(pub parser);
+
+
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "typhoon")]
+struct Opt {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+    #[structopt(short, long)]
+    debug: bool,
+}
+impl Default for Opt {
+    fn default() -> Self {
+        Self { debug: true }
+    }
+}
 
 fn main() {
-    dbg!(parser::ExprParser::new().parse("((22+2)*3%2)|1"));
-    dbg!(parser::ExprParser::new().parse("22"));
-    dbg!(parser::ExprParser::new().parse("a"));
-    dbg!(parser::ExprParser::new().parse("_a+2"));
-    dbg!(parser::StatementParser::new().parse("let b : i32 = a+2"));
-    dbg!(parser::StatementParser::new().parse("let a :i32 = ((22+dDS)*3%c)|1"));
+    let opt = Opt::from_args();
 
+    let x3 = "((22+2)*3%2)|1";
+    dbg!(&x3);
+    let x1: Box<Expr> = parser::ExprParser::new().parse(x3).unwrap();
+    // dbg!(parser::ExprParser::new().parse("22"));
+    // dbg!(parser::ExprParser::new().parse("a"));
+    // dbg!(parser::ExprParser::new().parse("_a+2"));
+    // dbg!(parser::StatementParser::new().parse("let b : i32 = a+2"));
+    // dbg!(parser::StatementParser::new().parse("let a :i32 = ((22+dDS)*3%c)|1"));
+
+    dbg!(&x1);
     unsafe {
         let context = core::LLVMContextCreate();
-        let module = core::LLVMModuleCreateWithName(c_str!("test"));
+        let module = core::LLVMModuleCreateWithName(c_str!("typhoon"));
         let builder = core::LLVMCreateBuilderInContext(context);
 
         let int_type = core::LLVMInt32TypeInContext(context);
@@ -41,65 +63,68 @@ fn main() {
 
         let bb = core::LLVMAppendBasicBlockInContext(context, function, c_str!("entry"));
         core::LLVMPositionBuilderAtEnd(builder, bb);
+        let x2 = x1.codegen(context, builder);
+        core::LLVMBuildRet(builder, x2);
 
-        let int_value = 100;
+        if opt.debug {
+            // emit llir
 
-        let int = core::LLVMConstInt(int_type, int_value, 0);
-        core::LLVMBuildRet(builder, int);
+            let string = LLVMPrintModuleToString(module);
 
-        // compile to object
-        let triple = LLVMGetDefaultTargetTriple();
-        LLVM_InitializeAllTargetInfos();
-        LLVM_InitializeAllTargets();
-        LLVM_InitializeAllTargetMCs();
-        LLVM_InitializeAllAsmParsers();
-        LLVM_InitializeAllAsmPrinters();
-        // let target = LLVMGetFirstTarget();
-        // LLVMGetTargetFromTriple(triple, target, ptr::null_mut());
-        let mut target: LLVMTargetRef = std::mem::uninitialized();
-        LLVMGetTargetFromTriple(triple, &mut target, ptr::null_mut());
-        let opt_level = LLVMCodeGenOptLevel::LLVMCodeGenLevelLess;
-        let reloc_mode = LLVMRelocMode::LLVMRelocDefault;
-        let code_model = LLVMCodeModel::LLVMCodeModelDefault;
+            let x = CStr::from_ptr(string).to_str().unwrap();
+            println!("llir: \n {}", x);
+        } else {
+            // emit executable binary file
+            // compile to object
+            let triple = LLVMGetDefaultTargetTriple();
+            LLVM_InitializeAllTargetInfos();
+            LLVM_InitializeAllTargets();
+            LLVM_InitializeAllTargetMCs();
+            LLVM_InitializeAllAsmParsers();
+            LLVM_InitializeAllAsmPrinters();
+            // let target = LLVMGetFirstTarget();
+            // LLVMGetTargetFromTriple(triple, target, ptr::null_mut());
+            let mut target: LLVMTargetRef = std::mem::uninitialized();
+            LLVMGetTargetFromTriple(triple, &mut target, ptr::null_mut());
+            let opt_level = LLVMCodeGenOptLevel::LLVMCodeGenLevelLess;
+            let reloc_mode = LLVMRelocMode::LLVMRelocDefault;
+            let code_model = LLVMCodeModel::LLVMCodeModelDefault;
 
-        dbg!(&opt_level);
-        dbg!(&reloc_mode);
-        dbg!(&code_model);
-        dbg!(&triple);
-        let name = LLVMGetTargetName(target);
-        let x = CStr::from_ptr(name as *mut i8);
-        println!("target name: {:?}", x);
-        let target_machine = LLVMCreateTargetMachine(
-            target,
-            triple,
-            c_str!("x86-64"),
-            c_str!(""),
-            opt_level,
-            reloc_mode,
-            code_model,
-        );
-        let file_type = LLVMCodeGenFileType::LLVMObjectFile;
+            let name = LLVMGetTargetName(target);
+            let x = CStr::from_ptr(name as *mut i8);
+            println!("target name: {:?}", x);
+            let target_machine = LLVMCreateTargetMachine(
+                target,
+                triple,
+                c_str!("x86-64"),
+                c_str!(""),
+                opt_level,
+                reloc_mode,
+                code_model,
+            );
+            let file_type = LLVMCodeGenFileType::LLVMObjectFile;
 
-        let mut error_str = ptr::null_mut();
-        let output_file_name = CString::new("out.o").unwrap();
-        let ret = LLVMTargetMachineEmitToFile(
-            target_machine,
-            module,
-            output_file_name.as_ptr() as *mut i8,
-            file_type,
-            &mut error_str,
-        );
-        if ret == 1 {
-            let x = CStr::from_ptr(error_str);
-            panic!("compile failed: {:?}", x);
+            let mut error_str = ptr::null_mut();
+            let output_file_name = CString::new("out.o").unwrap();
+            let ret = LLVMTargetMachineEmitToFile(
+                target_machine,
+                module,
+                output_file_name.as_ptr() as *mut i8,
+                file_type,
+                &mut error_str,
+            );
+            if ret == 1 {
+                let x = CStr::from_ptr(error_str);
+                panic!("compile failed: {:?}", x);
+            }
+            let output = std::process::Command::new("cc")
+                .arg("out.o")
+                .arg("-o")
+                .arg("out")
+                .output()
+                .expect("error on executing linker cc");
+            // core::LLVMPrintModuleToFile(module, c_str!("out.ll"), ptr::null_mut());
         }
-        let output = std::process::Command::new("cc")
-            .arg("out.o")
-            .arg("-o")
-            .arg("out")
-            .output()
-            .expect("error on executing linker cc");
-        // core::LLVMPrintModuleToFile(module, c_str!("out.ll"), ptr::null_mut());
 
         core::LLVMDisposeBuilder(builder);
         core::LLVMDisposeModule(module);
